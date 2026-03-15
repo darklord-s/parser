c3ffc3c5acab2b361846fb67cb8cfd65eee901f7
Kirill S <hamster.in.sneakers@mail.ru>
2026-03-16 01:04:28 +0300

    Writing results to file just added

--- parser.py
+++ parser.py
@@ -6,14 +6,15 @@ class Scraper:
         self.site = site
 
     def scrape(self):
-
                 # Добавляем заголовки, чтобы имитировать браузер
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         }
         
         req = urllib.request.Request(self.site, headers=headers)
-        
+        with open('links.txt', "w") as title:
+            title.write(f'{self.site}\n\n')
+
         try:
             r = urllib.request.urlopen(req)
             html = r.read()
@@ -24,6 +25,8 @@ class Scraper:
                 if url is None:
                     continue
                 if url.endswith('html'):
+                    with open('links.txt', "a") as f:
+                        f.write('\n' + url)
                     print('\n' + url)
         except Exception as e:
             print(f'Ошибка {e}')
